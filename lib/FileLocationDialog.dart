b110d41f5da3a514e3e1fa1be7165791d5b47d12
Ilesh <ileshkt@gmail.com>
2021-01-27 18:13:16 +0800

    Fixed weird alignment of FileLocationDialog.

--- lib/FileLocationDialog.dart
+++ lib/FileLocationDialog.dart
@@ -29,37 +29,40 @@ class _FileLocationDialogState extends State<FileLocationDialog> {
       content: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          RichText(
-            textAlign: TextAlign.left,
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: 'To view the file, tap ',
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                TextSpan(
-                  text: 'here',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                    letterSpacing: 0.15,
-                    color: Colors.blue,
+          // Wrapped in align because textAlign in RichText in a Column doesn't work
+          Align(
+            alignment: Alignment.centerLeft,
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'To view the file, tap ',
+                    style: Theme.of(context).textTheme.subtitle1,
                   ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      launch(
-                        "https://docs.google.com/spreadsheets/d/${widget.fileID}/",
-                      );
-                    },
-                ),
-              ],
+                  TextSpan(
+                    text: 'here',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.normal,
+                      letterSpacing: 0.15,
+                      color: Colors.blue,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        launch(
+                          "https://docs.google.com/spreadsheets/d/${widget.fileID}/",
+                        );
+                      },
+                  ),
+                ],
+              ),
             ),
           ),
           Text(""),
           Text(
-            "To access the file itself:\n- Open Google Drive\n- Go to My Drive\n- Go to a folder called 'BP Logger'\n- Open the file called 'log' to see your data.\nMake sure you are signed into the same Google account in Drive that you used in BP Logger",
+            "To access the file itself:\n- Open Google Drive\n- Go to My Drive\n- Go to a folder called 'BP Logger'\nThe file there called 'log' is where your data is stored.\n\nMake sure you are signed into the same Google account in Drive that you used in BP Logger",
             style: Theme.of(context).textTheme.subtitle1,
           ),
           Padding(
